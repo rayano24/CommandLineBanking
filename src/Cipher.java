bd1c4e8989e0aaaaa42ccfdc726a0f1f3f740a0d
Rayan Osseiran <10555572+rayano24@users.noreply.github.com>
2018-02-06 17:19:58 -0500

    Update Cipher.java

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,5 +1,3 @@
-/* Rayan Osseiran (feel free to remove my name, its here because the ciphers were learnt in class and I don't want to risk being 
- called out for plagiarizing myself if the automated software finds my GitHub */
 // Unrealistic implementation of encryption
 // Currently basic caesar cipher, to be updated. this only exists because I already have the code on hand.
 // Will be implemented when the project makes use of file I/O
